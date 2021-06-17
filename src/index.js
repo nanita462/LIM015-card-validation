c1daaec97da09ebfe03c41a4fe3c7833941be84f
Ana Aguilera <nanita462@gmail.com>
2021-06-17 08:28:36 -0500

    Prueba commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
+
+let creditCardNumbers = document.getElementById('creditCardNumbers')
+
+const clickAceptar= document.getElementById('aceptar')
+clickAceptar.addEventListener('click', verificacion)
+clickAceptar.addEventListener('click', validar)
+clickAceptar.addEventListener('click', enmascarar)
+
+function verificacion(){
+       
+    if (creditCardNumbers.value=='') {
+        alert('Debes ingresar un numero de tarjeta')
+        return false
+ 
+   
+ }
+}
+ 
+ //Input targeta
+creditCardNumbers.addEventListener('keyup', (e) =>{
+    let valInput=e.target.value;
+     creditCardNumbers.value= valInput
+     .replace(/\s/g,'')
+     .replace(/\D/g,'')
+     .replace(/([0-9]{4})/g,'$1 ')
+     .trim();
+
+} );
+
+//validar targeta
+
+function validar (){
+    let numeroTarjeta= creditCardNumbers.value;
+    let valida= (validator.isValid(numeroTarjeta));
+
+    //console.log(typeof valida)
+    if(valida==true){
+        alert('Tarjeta Valida')
+    }
+    else{
+        alert('Tarjeta Invalida')
+    }
+
+    
+
+}
+
+function enmascarar() {
+    let num= creditCardNumbers.value;
+    let mascara=(validator.maskify(num));
+    creditCardNumbers.value=mascara;
+    
+}
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,90 @@
+
 const validator = {
-  // ...
+  isValid:function(creditCardNumbers){
+    const aArreglo= Array.from(creditCardNumbers)
+    const aNumeros= aArreglo.map(i=>Number(i));
+    //let tarjetaValida;
+          
+   //console.log(aArreglo)
+   //console.log(aNumeros)
+   //algoritmo de luhn
+   const numeroInvertido= aNumeros.reverse()
+   //console.log(numeroInvertido)
+  
+   const numerosPares=numeroInvertido.filter(function(elemento, index) {    //obteniendo numeros pares
+       return(index%2)=== 1
+   })
+   //console.log(numerosPares)
+
+   const numerosImPares=numeroInvertido.filter(function(elemento, index) {    //obteniendo numeros Impares
+    return(index%2)=== 0
+   })
+   //console.log(numerosImPares)
+
+   const acumular= (acumulador,numero)=>acumulador+numero;
+
+   const numerosImparesR=numerosImPares.reduce(acumular);
+   //console.log(numerosImparesR)
+
+   const porDos=numerosPares.map(function(x){
+     return(x*2);
+   })
+   //console.log(porDos)
+   const menoresDeDiez= porDos.filter(porDos=>porDos<10);
+   //console.log(menoresDeDiez)
+   const menoresDeDiezR= menoresDeDiez.length> 0 ? menoresDeDiez.reduce(acumular) : 0;
+   //console.log(menoresDeDiezR)
+   const mayoresDeDiez= porDos.filter(porDos=>porDos>=10);
+   //console.log(mayoresDeDiez)
+
+   const restaMayoresDeDiez= mayoresDeDiez.map(function(x){
+     return(x-9);
+   })
+   //console.log(restaMayoresDeDiez);
+
+   
+   let restaMayoresDeDiezR=restaMayoresDeDiez.length> 0 ? restaMayoresDeDiez.reduce(acumular) : 0;
+   //console.log(restaMayoresDeDiezR)
+
+   const sumaTotal1= menoresDeDiezR+restaMayoresDeDiezR
+   //console.log(sumaTotal1)
+
+   const sumaTotal2= sumaTotal1+numerosImparesR
+   //console.log(sumaTotal2)
+
+   
+   if ((sumaTotal2 % 10 ==0)) {
+    return true;
+   } else {
+    return false;
+   }
+   //return tarjetaValida;
+   
+  },
+  
+  maskify: function(creditCardNumbers){
+
+    if(creditCardNumbers.length<4){
+      return creditCardNumbers
+    }else{
+      let last4=''
+      for(let i=creditCardNumbers.length-4; i < creditCardNumbers.length; i++){
+        last4 += creditCardNumbers[i]
+      }
+      let mask=''
+      for(let j=0; j<creditCardNumbers.length-4; j++){
+        mask +='#'
+      }
+      return mask+last4
+    }
+    }
+
+  
+       
+    
+   
+  
+  
 };
 
 export default validator;
